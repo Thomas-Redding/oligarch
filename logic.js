afc2bdb85da31c79140208dde6171132993ff6a2
tginart <tginart@stanford.edu>
2020-09-04 14:12:51 -0700

    ez merge

---
+++ logic.js
@@ -0,0 +1,21 @@
+let foo = require('./neighbors.js')
+console.log(foo)
+
+
+function initGameState()
+{
+    let foo = require('./neighbors.js')
+    console.log(foo)
+
+}
+
+
+
+function sendFile(response, filePath) 
+{
+    console.log("js is gay")
+    
+}
+
+initGameState();
+sendFile();
