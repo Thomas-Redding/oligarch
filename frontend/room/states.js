5999480168183a178745cfddbdc0cb368bf89a59
Morgan Redding <morganfredding@gmail.com>
2020-09-04 12:36:51 -0700

    Add initial frontend code

---
+++ frontend/room/states.js
@@ -0,0 +1,161 @@
+const countries = {
+  "Africa": {
+    "color": new Color(224, 128, 96)
+  },
+  "North America": {
+    "color": new Color(224, 224, 96)
+  },
+  "South America": {
+    "color": new Color(96, 255, 255)
+  },
+  "Europe": {
+    "color": new Color(96, 128, 255)
+  },
+  "Asia": {
+    "color": new Color(96, 255, 96)
+  },
+  "Australia": {
+    "color": new Color(255, 96, 255)
+  }
+};
+
+const states = {
+  "Alaska": {
+    "center": [64, 123],
+    "name": "Alaska"
+  },
+  "Northwest Territory": {
+    "center": [178, 130],
+    "name": "NW Territory"
+  },
+  "Greenland": {
+    "center": [396, 80],
+  },
+  "Alberta": {
+    "center": [167, 179]
+  },
+  "Ontario": {
+    "center": [240, 189]
+  },
+  "Quebec": {
+    "center": [313, 189]
+  },
+  "Western United States": {
+    "center": [160, 241],
+    "name": "W United States"
+  },
+  "Eastern United States": {
+    "center": [255, 258],
+    "name": "E United States"
+  },
+  "Western Europe": {
+    "center": [437, 279],
+    "name": "W Europe"
+  },
+  "Great Britain": {
+    "center": [426, 208]
+  },
+  "Northern Europe": {
+    "center": [490, 224],
+    "name": "N Europe"
+  },
+  "Southern Europe": {
+    "center": [502, 272],
+    "name": "S Europe"
+  },
+  "Ukraine": {
+    "center": [586, 184]
+  },
+  "Scandinavia": {
+    "center": [492, 130]
+  },
+  "Ural": {
+    "center": [682, 168]
+  },
+  "Siberia": {
+    "center": [747, 137]
+  },
+  "Yakutsk": {
+    "center": [848, 125]
+  },
+  "Kamchatka": {
+    "center": [942, 130]
+  },
+  "Irkutsk": {
+    "center": [813, 189]
+  },
+  "Mongolia": {
+    "center": [836, 240]
+  },
+  "Japan": {
+    "center": [906, 264]
+  },
+  "China": {
+    "center": [766, 302]
+  },
+  "Central America": {
+    "center": [190, 314],
+    "name": "Mexico"
+  },
+  "Venezuela": {
+    "center": [225, 385]
+  },
+  "Peru": {
+    "center": [211, 458]
+  },
+  "Brazil": {
+    "center": [317, 445]
+  },
+  "North Africa": {
+    "center": [439, 397],
+    "name": "N Africa"
+  },
+  "Egypt": {
+    "center": [514, 355]
+  },
+  "East Africa": {
+    "center": [552, 430],
+    "name": "E Africa"
+  },
+  "Congo": {
+    "center": [506, 483]
+  },
+  "Middle East": {
+    "center": [588, 333]
+  },
+  "India": {
+    "center": [704, 362]
+  },
+  "Afghanistan": {
+    "center": [657, 251]
+  },
+  "Indonesia": {
+    "center": [811, 454]
+  },
+  "New Guniea": {
+    "center": [912, 463]
+  },
+  "South Africa": {
+    "center": [506, 575]
+  },
+  "Western Australia": {
+    "center": [790, 553],
+    "name": "W Australia"
+  },
+  "Eastern Australia": {
+    "center": [912, 555],
+    "name": "E Australia"
+  },
+  "Argentina": {
+    "center": [244, 556]
+  },
+  "Iceland": {
+    "center": [432, 148]
+  },
+  "Siam": {
+    "center": [781, 372]
+  },
+  "Madagascar": {
+    "center": [588, 567],
+  }
+};
