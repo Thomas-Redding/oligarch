cce9177c69f06892edc5446f8fc49d647ec667aa
tginart <tginart@stanford.edu>
2020-09-05 14:31:33 -0700

    fix empty votes

--- logic.js
+++ logic.js
@@ -432,9 +432,8 @@ class Game
 
     _register_vote(username, candidate_username)
     {
+        this.mother_state.players[username].vote = candidate_username
         let candidate_votes = utils.candidate_votes(this.mother_state)
-        console.log('log')
-        console.log(candidate_votes)
         this._prayer('vote_tallied', candidate_votes)
         let n_votes = 0
         for (let player in candidate_votes) {
