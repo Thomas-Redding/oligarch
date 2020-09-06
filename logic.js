e15539812facfe2765fe390b6aaa36b4d0d9efe2
Thomas Redding <thomasredding@DN51s09k.SUNet>
2020-09-05 19:10:54 -0700

    Fix bug clock bug.

--- logic.js
+++ logic.js
@@ -581,8 +581,8 @@ class Game
         let tau = this.timer.queryTime() + TIMING.actions
         details['time'] = tau
         this.mother_state.clock = tau
-        this._prayer('begin_presidential_command',details)
         this.timer.start(tau, this._end_presidential_command.bind(this))
+        this._prayer('begin_presidential_command',details)
     }
 
     _end_presidential_command()
