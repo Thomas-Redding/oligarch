88049993257ccbfa96cee4c25d5b055cbed4082a
Thomas Redding <thomasredding@DN51s466.SUNet>
2020-09-05 22:37:50 -0700

    fix timer bug

--- logic.js
+++ logic.js
@@ -15,7 +15,7 @@ const TURNS = ['North America', 'South America',
     'Europe', 'Africa', 'Asia', 'Australia']
 const SUBPHASES = [null,'Election','Move','Attack','Spawn','Build','Dividends']
 const BLACKLISTED_NAMES = ['NA','SA','EU','AF','AS','AU']
-const TIMING = {'deliberation' : 90*1000, 'bidding' : 1*1000,
+const TIMING = {'deliberation' : 3*1000, 'bidding' : 1*1000,
  'election':10*1000, 'actions':10*1000}
 const UNITS = ['Cavalry','Infantry','Artillery']
 const COSTS = {'factory' : 10, 'barracks' : 15, 'Infantry': 10, 
