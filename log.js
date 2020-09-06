a066036803dc4d71ddb3aadddf2c738accf22160
Thomas Redding <thomasredding@DN51s466.SUNet>
2020-09-05 23:28:24 -0700

    Add log.js.

---
+++ log.js
@@ -0,0 +1,38 @@
+
+function log(s) {
+  if (log.enable === undefined) log.enable = false;
+  if (!log.enable) return false;
+  console.log(time(), ...arguments);
+}
+
+function time() {
+  let date = new Date();
+  let m = date.getMinutes() + "";
+  let s = date.getSeconds() + "";
+  let ms = new Date().getMilliseconds() + "";
+  m = m.padStart(2, "0");
+  s = s.padStart(2, "0");
+  ms = ms.padStart(3, "0");
+  return m + ":" + s + "." + ms;
+}
+
+function get_stack() {
+  try {
+    var a = {};
+    a.debug();
+  } catch(e) {
+    let stack1 = e.stack.split("\n").slice(2);
+    let stack2 = [];
+    for (let s1 of stack1) {
+      let s2 = s1.substr(7);
+      let methodName = s2.substr(0, s2.indexOf(" "));
+      if (methodName.startsWith("WebSocket")) break;
+      if (methodName.startsWith("tryOnTimeout")) break;
+      stack2.push(methodName);
+    }
+    return stack2;
+  }
+}
+
+module.exports = log
+
