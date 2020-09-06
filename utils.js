119ef7b8086e97a2baf7424803b4b72079986fdd
tginart <tginart@stanford.edu>
2020-09-05 17:02:10 -0700

    fix utils

--- utils.js
+++ utils.js
@@ -433,7 +433,7 @@ let utils = {
   },
 
   territories_of_nation_that_can_spawn: (mother_state, nation) => {
-    let territory_names = utils.territories_of_nation(mother_state, nations);
+    let territory_names = utils.territories_of_nation(mother_state, nation);
     let rtn = [];
     for (let territory_name of territory_names) {
       let territory = utils.territory_for_territory_name(mother_state, territory_name);
