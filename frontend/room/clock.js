2eb8b9ec7beeecdecaa480f5452c4440e7a827dd
Morgan Redding <morganfredding@gmail.com>
2020-10-31 10:02:32 -0500

    Move js out of index.html and use typescript for clock.js

--- frontend/room/clock.js
+++ frontend/room/clock.js
@@ -1,42 +1,45 @@
 // Singleton class.
-class Clock {
-  constructor() {
-    this._startTime = new Date();
-    this._totalTime = 0;
-    this._paused = false;
-    setInterval(() => {
-      this.update();
-    }, 100);
-  }
-  set_time_remaining(timeRemaining) {
-    this._startTime = new Date();
-    this._totalTime = timeRemaining;
-    this._paused = false;
-    this.update();
-  }
-  pause() {
-    this._totalTime = this._totalTime - (new Date() - this._startTime);
-    this.startTime = new Date();
-    this._paused = true;
-  }
-  update() {
-    if (!statusBarTimeDiv) {
-      return;
+var Clock = (function () {
+    function Clock() {
+        var _this = this;
+        this._startTime = new Date().getTime();
+        this._totalTime = 0.0;
+        this._paused = false;
+        setInterval(function () {
+            _this.update();
+        }, 100);
     }
-    let timeElapsed = new Date() - this._startTime;
-    if (this._paused) {
-      timeElapsed = 0;
-    }
-    let secsRemaining = (this._totalTime - timeElapsed) / 1000;
-    secsRemaining = Math.max(secsRemaining, 0);
-    let minsRemaining = secsRemaining / 60 | 0;
-    secsRemaining -= minsRemaining * 60;
-    secsRemaining = Math.floor(secsRemaining) + '';
-    if (secsRemaining.length === 1) {
-      secsRemaining = '0' + secsRemaining;
-    }
-    statusBarTimeDiv.innerHTML = minsRemaining + ':' + secsRemaining;
-  }
-}
-
-let gClock = new Clock();
+    Clock.prototype.set_time_remaining = function (timeRemaining) {
+        this._startTime = new Date().getTime();
+        this._totalTime = timeRemaining;
+        this._paused = false;
+        this.update();
+    };
+    Clock.prototype.pause = function () {
+        var currentTime = new Date().getTime();
+        this._totalTime -= currentTime - this._startTime;
+        this._startTime = new Date().getTime();
+        this._paused = true;
+    };
+    Clock.prototype.update = function () {
+        var statusBarTimeDiv = document.getElementById("statusBarTimeDiv");
+        if (!statusBarTimeDiv) {
+            return;
+        }
+        var timeElapsed = new Date().getTime() - this._startTime;
+        if (this._paused) {
+            timeElapsed = 0;
+        }
+        var secsRemaining = (this._totalTime - timeElapsed) / 1000;
+        secsRemaining = Math.max(secsRemaining, 0);
+        var minsRemaining = secsRemaining / 60 | 0;
+        secsRemaining -= minsRemaining * 60;
+        var secsRemainingStr = Math.floor(secsRemaining) + '';
+        if (secsRemainingStr.length === 1) {
+            secsRemainingStr = '0' + secsRemainingStr;
+        }
+        statusBarTimeDiv.innerHTML = minsRemaining + ':' + secsRemainingStr;
+    };
+    return Clock;
+})();
+var gClock = new Clock();
---
+++ frontend/room/ts/clock.ts
@@ -0,0 +1,47 @@
+// Singleton class.
+class Clock {
+  _startTime: number;
+  _totalTime: number;
+  _paused: boolean;
+  constructor() {
+    this._startTime = new Date().getTime();
+    this._totalTime = 0.0;
+    this._paused = false;
+    setInterval(() => {
+      this.update();
+    }, 100);
+  }
+  set_time_remaining(timeRemaining : number) {
+    this._startTime = new Date().getTime();
+    this._totalTime = timeRemaining;
+    this._paused = false;
+    this.update();
+  }
+  pause() {
+    let currentTime : number = new Date().getTime();
+    this._totalTime -= currentTime - this._startTime;
+    this._startTime = new Date().getTime();
+    this._paused = true;
+  }
+  update() {
+    let statusBarTimeDiv : HTMLElement = document.getElementById("statusBarTimeDiv");
+    if (!statusBarTimeDiv) {
+      return;
+    }
+    let timeElapsed : number = new Date().getTime() - this._startTime;
+    if (this._paused) {
+      timeElapsed = 0;
+    }
+    let secsRemaining : number = (this._totalTime - timeElapsed) / 1000;
+    secsRemaining = Math.max(secsRemaining, 0);
+    let minsRemaining = secsRemaining / 60 | 0;
+    secsRemaining -= minsRemaining * 60;
+    let secsRemainingStr = Math.floor(secsRemaining) + '';
+    if (secsRemainingStr.length === 1) {
+      secsRemainingStr = '0' + secsRemainingStr;
+    }
+    statusBarTimeDiv.innerHTML = minsRemaining + ':' + secsRemainingStr;
+  }
+}
+
+let gClock = new Clock();
